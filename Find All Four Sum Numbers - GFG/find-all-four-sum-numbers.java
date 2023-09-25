116a02ddfd6076543b1b73b0d873c3e1205146f9
BRAJESH KUMAR MISHRA <81823542+Br8298469582@users.noreply.github.com>
2023-09-26 00:30:36 +0530

    Added solution - LeetHub

---
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.java
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            String[] nk = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            Solution sln = new Solution();
+            ArrayList<ArrayList<Integer>> ans = sln.fourSum(a, k);
+            for (ArrayList<Integer> v : ans) {
+                for (int u : v) {
+                    System.out.print(u + " ");
+                }
+                System.out.print("$");
+            }
+            if (ans.isEmpty()) {
+                System.out.print(-1);
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// arr[] : int input array of integers
+// k : the quadruple sum required
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] nums, int target) {
+        // code here
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+         int n = nums.length;
+        if (n < 4) {
+            return ans; // If there are less than 4 elements, no valid quadruplets can be formed.
+        }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            // If the current nums[i] * 4 is greater than the target, break the loop
+            if (nums[i] * 4 > target) {
+                break;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        ArrayList<Integer> combination = new ArrayList<>();
+                        combination.add(nums[i]);
+                        combination.add(nums[j]);
+                        combination.add(nums[left]);
+                        combination.add(nums[right]);
+                        ans.add(combination);
+
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
